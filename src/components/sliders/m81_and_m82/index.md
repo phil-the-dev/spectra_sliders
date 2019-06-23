44708135e88c32659126289d952589ca88abc3a0
Philippe Batigne <pbatigne@gmail.com>
2019-06-23 18:00:44 -0400

    adding slider mechanism demo

--- src/components/sliders/m81_and_m82/index.md
+++ src/components/sliders/m81_and_m82/index.md
@@ -7,8 +7,13 @@ images: [
     wavelength: visible,
     description: this is visible,
     credits: NASA/JPL-Caltech/UCLA.
+  },
+  {
+    img: ./infrared.jpg,
+    wavelength: visible,
+    description: this is not visible,
+    credits: NASA/JPL-Caltech/UCLA.
   }
 ]
-credits: 
 ---
 The M81 and M82 galaxies lie close to one another in the sky but are strikingly different, as revealed in the infrared view. The blindingly bright glow of M82 in the infrared vastly outshines its neighbor, due to the phenomenally strong rate of star formation in its core. Such activity is more evident when you can see the two objects on the same brightness scale, as shown here. 
--- src/components/templates/slider-post.js
+++ src/components/templates/slider-post.js
@@ -1,12 +1,43 @@
 import React from "react"
 import { graphql } from 'gatsby'
+import Img from "gatsby-image";
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
-const SliderPostTemplate = ({ data: { post } }) => (
-  <div>
-    <h1>{post.title}</h1>
-    <div dangerouslySetInnerHTML={{ __html: post.html }} />
-  </div>
-)
+
+// const SliderPostTemplate = ({ data: { post } }) => (
+// )
+class SliderPostTemplate extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      opacity: 1
+    }
+  }
+
+  SliderChange = (event) => {
+    this.setState({
+      opacity: event
+    })
+  }
+
+
+  render() {
+    const { post } = this.props.data
+    console.log(this.state)
+    let opac = this.state.opacity;
+    return (
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        {/* <div>{post.frontmatter.images.map((i) => { return i.description })}</div> */}
+        {post.frontmatter.images.map((i) => { return (<div style={{ opacity: opac / 100 }} > <Img fluid={i.img.childImageSharp.fluid} /> </div>) })}
+        <Slider onChange={this.SliderChange.bind(this)} />
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    )
+  }
+}
+
 
 export default SliderPostTemplate
 
@@ -16,6 +47,19 @@ export const pageQuery = graphql`
             html
             frontmatter {
                 title
+                images{
+                  img{
+                    childImageSharp {
+                      resize(width: 150, height: 150) {
+                        src
+                      }
+                      fluid(maxWidth: 150) {
+                        ...GatsbyImageSharpFluid
+                      }
+                    }
+                  }
+                  description
+                }
             }
         }
     }
